e1f20a6ffbcd04376f931136d05fc87f229a07ed
gallegodev <gallegodev1991@gmail.com>
2023-01-29 00:02:01 +0000

    Add note to function show_board, because is not working properly

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ player = input("Hey YOU!!! Whats your name?\n")
 
 player_answer = input(f"Hi {player}, do you want to play a game of Tic Tac Toe against me?\n")
 
-
+#function not working / find out why
 def show_board(board):
     print(" {} | {} | {} ".format(board[1], board[2], board[3]))
     print("---+---+---")
