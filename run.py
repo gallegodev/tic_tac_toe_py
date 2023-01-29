8c9c562766e8ec7f869faffb8bf1f9ca97651846
gallegodev <gallegodev1991@gmail.com>
2023-01-29 00:46:44 +0000

    Add function play_game and debug function show_board

--- run.py
+++ run.py
@@ -2,15 +2,27 @@
 
 player = input("Hey YOU!!! Whats your name?\n")
 
-player_answer = input(f"Hi {player}, do you want to play a game of Tic Tac Toe against me?\n")
+print(f"Hi {player}, lets play a game of Tic Tac Toe against me. \n")
+print("I bet that even brilliant the way u r I can beat you")
 
-#function not working / find out why
 
-print(" {} | {} | {} ")
-print("---+---+---")
-print(" {} | {} | {} ")
-print("---+---+---")
-print(" {} | {} | {} ")
-print("---+---+---")
-    
+def show_board(board):
+    print(" {} | {} | {} ".format(board[1], board[2], board[3]))
+    print("---+---+---")
+    print(" {} | {} | {} ".format(board[4], board[5], board[6]))
+    print("---+---+---")
+    print(" {} | {} | {} ".format(board[7], board[8], board[9]))
 
+
+def play_game():
+    board = [" " for x in range(10)]
+    while True:
+        show_board(board)
+        user_move = int(input("Enter your move (1-9): "))
+        if board[user_move] == " ":
+            board[user_move] = "X"
+        else:
+            print("Invalid move. Try again.")
+            continue
+
+play_game()
